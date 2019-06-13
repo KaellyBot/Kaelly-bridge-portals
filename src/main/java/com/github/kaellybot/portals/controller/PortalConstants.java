88f8d34d477165ef7c4bd7808d291e18c43a4e80
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-06-14 01:48:22 +0200

    REST Endpoints errors managed and reported (not found parameters and unknown exceptions)

--- src/main/java/com/github/kaellybot/portals/controller/PortalConstants.java
+++ src/main/java/com/github/kaellybot/portals/controller/PortalConstants.java
@@ -1,5 +1,8 @@
 package com.github.kaellybot.portals.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 final class PortalConstants {
 
     private PortalConstants(){}
@@ -9,4 +12,11 @@ final class PortalConstants {
     static final String TOKEN_VAR = "token";
     static final String DIMENSION_VAR = "dimension";
     static final String SERVER_VAR = "{server}";
+
+    static final ResponseStatusException SERVER_NOT_FOUND = new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "The specified server is not found.");
+    static final ResponseStatusException DIMENSION_NOT_FOUND = new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "The specified dimension is not found.");
+    static final ResponseStatusException INTERNAL_SERVER_ERROR = new ResponseStatusException(
+            HttpStatus.INTERNAL_SERVER_ERROR, "A technical error occurred. Sorry for the inconvenience.");
 }
--- src/main/java/com/github/kaellybot/portals/controller/PortalController.java
+++ src/main/java/com/github/kaellybot/portals/controller/PortalController.java
@@ -5,9 +5,14 @@ import com.github.kaellybot.portals.model.constants.Dimension;
 import com.github.kaellybot.portals.model.constants.Server;
 import com.github.kaellybot.portals.model.dto.ExternalPortalDto;
 import com.github.kaellybot.portals.model.dto.PortalDto;
+import com.github.kaellybot.portals.service.IDimensionService;
 import com.github.kaellybot.portals.service.IPortalService;
+import com.github.kaellybot.portals.service.IServerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -17,51 +22,63 @@ import static com.github.kaellybot.portals.controller.PortalConstants.*;
 @RequestMapping(API)
 public class PortalController {
 
-    private IPortalService service;
+    static final Logger LOG = LoggerFactory.getLogger(PortalController.class);
+    private IPortalService portalService;
+    private IServerService serverService;
+    private IDimensionService dimensionService;
 
-    public PortalController(IPortalService service){
-        this.service = service;
+    public PortalController(IPortalService portalService, IServerService serverService, IDimensionService dimensionService){
+        this.portalService = portalService;
+        this.serverService = serverService;
+        this.dimensionService = dimensionService;
     }
 
     @GetMapping(path = SERVER_VAR + PORTALS, params = { DIMENSION_VAR, TOKEN_VAR })
     public Mono<PortalDto> findById(@PathVariable(value="server") String serverName,
-                                    @RequestParam(value="dimension") String dimensionName, @RequestParam String token){
-        // TODO
-        Server server = Server.valueOf(serverName.toUpperCase());
-        if (server == null){
-            // TODO
+                                    @RequestParam(value="dimension") String dimensionName,
+                                    @RequestParam String token){
+        try {
+            Server server = serverService.findByName(serverName).orElseThrow(() -> SERVER_NOT_FOUND);
+            Dimension dimension = dimensionService.findByName(dimensionName).orElseThrow(() -> DIMENSION_NOT_FOUND);
+            return portalService.findById(server, dimension)
+                    .map(PortalMapper::map);
+        } catch(ResponseStatusException e){
+            throw e;
+        } catch(Exception e){
+            LOG.error("findById", e);
+            throw INTERNAL_SERVER_ERROR;
         }
-
-        Dimension dimension = Dimension.valueOf(dimensionName.toUpperCase());
-        if (dimension == null){
-            // TODO
-        }
-
-        return service.findById(server, dimension)
-                .map(PortalMapper::map);
     }
 
     @GetMapping(path = SERVER_VAR + PORTALS, params = { TOKEN_VAR })
     public Flux<PortalDto> findAllByPortalIdServer(@PathVariable(value="server") String serverName,
                                                    @RequestParam String token){
-        // TODO
-        Server server = Server.valueOf(serverName.toUpperCase());
-        if (server == null){
-            // TODO
+        try {
+            Server server = serverService.findByName(serverName).orElseThrow(() -> SERVER_NOT_FOUND);
+            return portalService.findAllByPortalIdServer(server)
+                    .map(PortalMapper::map);
+        } catch(ResponseStatusException e){
+            throw e;
+        } catch(Exception e){
+            LOG.error("findAllByPortalIdServer", e);
+            throw INTERNAL_SERVER_ERROR;
         }
-        return service.findAllByPortalIdServer(server)
-                .map(PortalMapper::map);
     }
 
     @PostMapping(path= SERVER_VAR + PORTALS, params = { DIMENSION_VAR, TOKEN_VAR }, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> addPortal(@PathVariable(value="server") String serverName, @RequestParam String dimension,
-                                       @RequestParam String token, @RequestBody ExternalPortalDto coodinates){
-        // TODO
-        Server server = Server.valueOf(serverName.toUpperCase());
-
-        if (server == null){
+    public Mono<String> addPortal(@PathVariable(value="server") String serverName,
+                                  @RequestParam String dimension,
+                                  @RequestParam String token,
+                                  @RequestBody ExternalPortalDto coodinates){
+        try {
+            Server server = serverService.findByName(serverName).orElseThrow(() -> SERVER_NOT_FOUND);
             // TODO
+            return Mono.empty();
+        } catch(ResponseStatusException e){
+            throw e;
+        } catch(Exception e){
+            LOG.error("addPortal", e);
+            throw INTERNAL_SERVER_ERROR;
         }
-        return Mono.empty();
     }
 }
