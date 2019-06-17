0102059180e4eacb24a2ea1ef155ffd7ef4ef063
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-06-17 23:41:07 +0200

    PortalConstants tests added

--- src/main/java/com/github/kaellybot/portals/controller/PortalConstants.java
+++ src/main/java/com/github/kaellybot/portals/controller/PortalConstants.java
@@ -12,11 +12,15 @@ final class PortalConstants {
     static final String TOKEN_VAR = "token";
     static final String DIMENSION_VAR = "dimension";
     static final String SERVER_VAR = "{server}";
+    static final String SERVER_NOT_FOUND_MESSAGE = "The specified server is not found.";
+    static final String DIMENSION_NOT_FOUND_MESSAGE = "The specified dimension is not found.";
+    static final String INTERNAL_SERVER_ERROR_MESSAGE = "A technical error occurred. Sorry for the inconvenience.";
+
 
     static final ResponseStatusException SERVER_NOT_FOUND = new ResponseStatusException(
-            HttpStatus.NOT_FOUND, "The specified server is not found.");
+            HttpStatus.NOT_FOUND, SERVER_NOT_FOUND_MESSAGE);
     static final ResponseStatusException DIMENSION_NOT_FOUND = new ResponseStatusException(
-            HttpStatus.NOT_FOUND, "The specified dimension is not found.");
+            HttpStatus.NOT_FOUND, DIMENSION_NOT_FOUND_MESSAGE);
     static final ResponseStatusException INTERNAL_SERVER_ERROR = new ResponseStatusException(
-            HttpStatus.INTERNAL_SERVER_ERROR, "A technical error occurred. Sorry for the inconvenience.");
+            HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MESSAGE);
 }
