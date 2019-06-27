4355c79b2480de0c83737cab4b6535e5c5c5f4de
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-06-27 20:22:37 +0200

    PortalApplication test added

---
+++ src/test/java/com/github/kaellybot/portals/PortalApplicationTest.java
@@ -0,0 +1,11 @@
+package com.github.kaellybot.portals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PortalApplication.class)
+class PortalApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
