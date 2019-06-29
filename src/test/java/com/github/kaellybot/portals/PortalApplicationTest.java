077d894bd689bafac39abde6b9ad771c91843bd7
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-06-29 16:14:40 +0200

    Smell code removed from PortalApplicationTest

--- src/test/java/com/github/kaellybot/portals/PortalApplicationTest.java
+++ src/test/java/com/github/kaellybot/portals/PortalApplicationTest.java
@@ -2,10 +2,12 @@ package com.github.kaellybot.portals;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class PortalApplicationTest {
 
     @Test
     void contextLoads() {
-        PortalApplication.main(new String[]{"--spring.main.web-environment=false"});
+        assertDoesNotThrow(() -> PortalApplication.main(new String[]{"--spring.main.web-environment=false"}));
     }
 }
